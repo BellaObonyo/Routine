575aca525827becb7a91dc0b23f37ccf189eca1d
BellaObonyo <cynthiabella.obonyo@gmail.com>
2023-02-03 16:04:31 +0300

    add login form

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello!</Text>
+      <Text>Enter your Email:</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#309cff',
     alignItems: 'center',
     justifyContent: 'center',
   },
